28866e920a101c7a73567c643fc3a3e66032228a
tingbing.ztb <tingbing.ztb@alibaba-inc.com>
2020-12-17 17:38:00 +0800

    x

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,72 +1,112 @@
-import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { FormattedMessage } from 'umi-plugin-react/locale';
-import { Table, Input } from 'antd';
-import { VTComponents } from 'virtualizedtableforantd';
-
-import styles from './Welcome.less';
+import React from 'react';
+import { Input } from 'antd';
 
 //acebookarchive/fixed-data-table
 
 // react-lazyload
-const MyCustomCell = ({ mySpecialProp }) => (
-  <Cell>{mySpecialProp === 'column2' ? "I'm column 2" : "I'm not column 2"}</Cell>
-);
+
 export default class Index extends React.PureComponent {
   state = {
-    rows: [],
+    renderData: [],
+    itemHeight: 60,
+    canSeeNums: 0,
+    head: 0,
+    tail: 10,
   };
   componentDidMount() {
-    const a = [];
-    for (let i = 0; i < 1000; i++) {
-      a.push({
-        name: 'ww',
-        id: i + 'iii',
+    this.createMockData();
+    this.refs.wrrapCpntainer.addEventListener('scroll', (e: any) => {
+      requestAnimationFrame(() => {
+        this.handSrcoll(e);
       });
-    }
-    this.setState({
-      rows: a,
     });
   }
 
-  onChange = (value: any, index: number) => {
-    const rows = [...this.state.rows];
-    rows[index]['name'] = value;
-    this.setState({ rows });
+  componentWillUnmount() {
+    // 去掉addEventListener
+  }
+
+  createMockData = () => {
+    let data = [];
+    for (var i = 0; i < 10000; i++) {
+      data.push({ name: 'ss', index: i });
+    }
+    this.setState({
+      renderData: data,
+    });
+    const { itemHeight } = this.state;
+    this.refs.scrollBody.style.height = 10000 * itemHeight + 'px';
+    const canSeeNums =
+      Math.ceil(Number(this.refs.wrrapCpntainer.style.height.split('px')[0]) / itemHeight) + 2;
+    this.setState({
+      tail: canSeeNums,
+      canSeeNums: canSeeNums, // 可展示的数量+2
+    });
   };
-  columns = [
-    { title: '序号', dataIndex: 'index', width: 50 },
-    {
-      title: '名称',
-      dataIndex: 'name',
-      width: 50,
-      render: (value: any, record: any, index: number) => (
-        <Input
-          style={{ width: '200px' }}
-          value={value}
-          onChange={e => {
-            this.onChange(e.target.value, index);
-          }}
-        />
-      ),
-    },
-  ];
+
+  onChange = (value: any, index: number) => {
+    const renderData: any = [...this.state.renderData];
+    renderData[index]['name'] = value;
+    this.setState({ renderData });
+  };
+
+  handSrcoll = (e: any) => {
+    const { itemHeight } = this.state;
+    if (e.target.scrollTop) {
+      this.setState({
+        head: Math.ceil((e.target.scrollTop - itemHeight + 10) / itemHeight),
+        tail: Math.ceil(e.target.scrollTop / itemHeight) + 11,
+      });
+    }
+  };
+
   render() {
-    const { rows } = this.state;
+    const { itemHeight, head, tail, renderData } = this.state;
     return (
       <PageHeaderWrapper>
-        <Table
-          className="templateTable"
-          dataSource={rows} //dataSource 是table的数据
-          // bordered
-          columns={this.columns}
-          pagination={false}
-          rowKey={record => record.id}
-          scroll={{ y: 500 }} //y: 500 it's important!!!
-          /*the id is immutable   the height prop is variable */
-          components={VTComponents({ id: 1000 })} // 这是最核心的代码
-        />
+        <div
+          style={{
+            width: 300,
+            height: 500,
+            border: '1px solid #ddd',
+            overflowY: 'scroll',
+            margin: '20px auto',
+          }}
+          ref="wrrapCpntainer"
+        >
+          {/** 滚动容器 */}
+          <div ref="scrollBody">
+            <div
+              style={{
+                transform: `translateY(${head * itemHeight}px)`,
+              }}
+            >
+              {renderData.slice(head, tail).map((item: any) => (
+                <div
+                  key={item.index}
+                  style={{
+                    height: '50px',
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <span style={{ margin: '0 20px' }}>{item.index + 1}</span>
+                  <Input
+                    style={{ width: '200px' }}
+                    value={item.name}
+                    onChange={e => {
+                      this.onChange(e.target.value, item.index);
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </PageHeaderWrapper>
     );
   }
 }
+
+/// 订单中心/拆单
